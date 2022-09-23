7d62f0f6a96cce026021190e3cba835812a88af4
khalid <khalid1alami1@gmail.com>
2022-09-23 11:23:14 +0200

    eerste commit

---
+++ module 2/deel 2/person.py
@@ -0,0 +1,14 @@
+from ctypes import addressof
+
+
+Naam=  input("wat is jouw naam")
+addres= input("wat is jouw adres")
+postcode= input("wat is jouw postcode")
+woonplaats= input("wat is jouw woonplaats")
+
+print("---------------------------------------------------")
+print(f"|  naam   : {Naam}                                ")
+print(f"|  adres  :  {addres}                             ")
+print(f"|  postcode   : {postcode}                        ")
+print(f"|  woonplaats  :  {woonplaats}                    ")
+print("---------------------------------------------------")
