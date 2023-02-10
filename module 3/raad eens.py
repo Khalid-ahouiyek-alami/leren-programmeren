ac38e851efc5fae17563cefb98a6ce8d1ad92a2d
khalid <khalid1alami1@gmail.com>
2023-02-11 00:07:46 +0100

    .

--- module 3/raad eens.py
+++ module 3/raad eens.py
@@ -3,39 +3,41 @@ import random
 geheimen_getal = random.randint(1, 1000)
 pogingen = 0
 score = 0
-round_count = 0
-
-while round_count < 20:
+ronde_teller = 0
+print(geheimen_getal)
+while ronde_teller < 20:
     geraden_getal = int(input("raad maar een nummer between 1 and 1000: "))
     pogingen += 1
 
     if geraden_getal == geheimen_getal:
         print("Correct! je hebt deze ronde gewonnen.")
         score += 1
-        round_count += 1
+        ronde_teller += 1
         geheimen_getal = random.randint(1, 1000)
         pogingen = 0
-        if round_count == 20:
+        if ronde_teller == 20:
             break
         nog_een_keer = input("wil je nog een ronde spelen? (j/n) ")
         if nog_een_keer == "n":
             break
     elif geraden_getal < geheimen_getal:
-        print("The nummer is hoger.") 
+        print("de nummer is hoger.") 
     else:
         print("de nummer is lager.")
 
-    if abs(geraden_getal - geheimen_getal) < 50:
-        print("je bent warm.")
+    
     if abs(geraden_getal - geheimen_getal) < 20:
         print("je bent heel warm.")
-
+    elif abs(geraden_getal - geheimen_getal) < 50:
+            print("je bent warm.")
+   
+   
     if pogingen == 10:
         print("je hebt de max pogingen voor deze ronden")
-        round_count += 1
+        ronde_teller += 1
         geheimen_getal = random.randint(1, 1000)
         pogingen = 0
-        if round_count == 20:
+        if ronde_teller == 20:
             break
         nog_een_keer = input("wil je nog een ronde spelen? (j/n) ")
         if nog_een_keer == "n":
---
+++ module 4/alle_dagen_van_de_week.py
@@ -0,0 +1,18 @@
+alle_dagen_van_de_week = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
+for s in range(7):
+    print(alle_dagen_van_de_week[s])
+print()
+for x in range(5):
+   print(alle_dagen_van_de_week[x])
+print()
+for a in range(5,7):
+    print(alle_dagen_van_de_week[a])
+print()
+for z in range(6, -1, -1):
+    print(alle_dagen_van_de_week[z])
+print()
+for e in range(4, -1, -1):
+    print(alle_dagen_van_de_week[e])
+print()
+for u in range(6, 4, -1):
+    print(alle_dagen_van_de_week[u])
